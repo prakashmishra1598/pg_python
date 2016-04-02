a5e9229ef88b83f02cee28efa5dee08ae0b5c406
Gaurav <gaurav@bloomreach.com>
2016-04-02 16:32:49 +0530

    Adding support for froup by clause in read

--- build/lib/pg_python/_read.py
+++ build/lib/pg_python/_read.py
@@ -1,7 +1,7 @@
-def make_postgres_read_statement(table, kv_map, keys_to_get, limit, order_by, order_type, debug=True):
+def make_postgres_read_statement(table, kv_map, keys_to_get, limit, order_by, order_type, debug, clause):
     _prefix = "SELECT"
     _table_string = " ".join(["FROM", table])
-    _key_string = " AND ".join([ k + "= %s" for k in kv_map.keys()])
+    _key_string = " AND ".join([ k + clause + "%s" for k in kv_map.keys()])
     statement = " ".join([_prefix, ", ".join(sorted(keys_to_get)), _table_string])
     if len(kv_map.keys()) > 0:
       statement = " ".join([_prefix, ", ".join(sorted(keys_to_get)), _table_string, "WHERE", _key_string])
