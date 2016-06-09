13f2ae94f705d8ca91057bfd97c74a00450b838b
Gaurav <gaurav@primenumbers.in>
2016-06-09 19:17:02 +0530

    Add postgres table

--- pg_python/pg_table.py
+++ pg_python/pg_table.py
@@ -4,12 +4,25 @@ Pure functional.
 """
 from BeautifulSoup import BeautifulSoup
 
+
 def _get_soup(html, elem):
+    """
+    Soup html element
+    :param html:
+    :param elem:
+    :return:
+    """
     soup = BeautifulSoup(html)
     tables = soup.findAll(elem)
     return tables
 
+
 def _get_fixed_soup(html):
+    """
+    Fix the soup
+    :param html:
+    :return:
+    """
     tables = _get_soup(html, 'table')
     if tables is not None and len(tables) > 0:
         return tables
@@ -19,12 +32,14 @@ def _get_fixed_soup(html):
         return _get_soup("<table>" + html + "</table>", 'table')
     return None
 
+
 def _get_header_cells(soup):
     all_th = []
     th = soup('th')
     for i in th:
         all_th.append(i.text)
     return all_th
+
 
 def _get_caption_cells(soup):
     all_th = []
@@ -61,3 +76,48 @@ def get_table(html):
         caption = _get_caption_cells(soup)
         all_tables.append({'header' : first_row, 'data': rows, 'title': caption})
     return count, all_tables
+
+
+def get_table_with_links(html):
+    """
+    Fetch all tables in the html that contain links,
+    returns a list of dictionaries with {header : [], data : [[]], num_links : 4}
+    :param html:
+    :return:
+    """
+    all_tables = []
+    soup = _get_fixed_soup(html)
+    if not soup:
+        return 0, None
+
+    count = 0
+    if type(soup) == list:
+        for s in soup:
+            rows = [[cell.text for cell in row("td")]
+                    for row in s("tr")]
+            links = [[cell.findAll('a') for cell in row("td")]
+                    for row in s("tr")]
+            count = count + 1
+            first_row = _get_header_cells(s)
+            caption = _get_caption_cells(s)
+            all_tables.append({'header': first_row, 'data': rows, 'title': caption, 'links': links})
+    else:
+        rows = [[cell.text for cell in row("td")]
+                for row in soup("tr")]
+
+        links = [[cell.findAll('a') for cell in row("td")]
+                 for row in soup("tr")]
+        count = count + 1
+        first_row = _get_header_cells(soup)
+        caption = _get_caption_cells(soup)
+        all_tables.append({'header': first_row, 'data': rows, 'title': caption, 'links' : links})
+    return count, all_tables
+
+if __name__ == "__main__":
+    import requests
+    r = requests.get("http://moud.gov.in/tenders")
+    num, tables = get_table_with_links(r.content)
+    print num
+    for table in tables:
+        for link in table["links"]:
+            print(link)
