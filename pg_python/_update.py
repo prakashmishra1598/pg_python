7a388bca0db43f700c63f08729a0b57e60d178ad
Gaurav <gaurav@primenumbers.in>
2016-06-20 22:49:44 +0530

    update fixxed where clause to be an AND

--- pg_python/_update.py
+++ pg_python/_update.py
@@ -1,6 +1,6 @@
 def make_postgres_update_statement(table, kv_map, where_kv_map, debug = True):
     _prefix = "UPDATE"
-    keys = ",".join([k + "=%s" for k in kv_map.keys()])
+    keys = " AND ".join([k + "=%s" for k in kv_map.keys()])
     where_keys = ",".join([k + "=%s" for k in where_kv_map.keys()])
     value_proxy_array = ["%s"] * len(kv_map)
     value_proxy_string = ", ".join(value_proxy_array)
