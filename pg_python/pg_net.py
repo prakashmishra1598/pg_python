8cd60edce2f816f399da56207cfe6c62829702cf
Gaurav <gaurav@primenumbers.in>
2016-09-03 14:29:33 +0530

    Adding proper domain to domain finder

--- pg_python/pg_net.py
+++ pg_python/pg_net.py
@@ -4,3 +4,11 @@ def get_domain(url):
     parsed_uri = urlparse(url)
     domain = ('{uri.netloc}/'.format(uri=parsed_uri)).strip("/")
     return domain
+
+def get_proper_domain(url):
+    d = get_domain(url)
+    prefix_remove = ["http://", "https://", "www."]
+    for prefix in prefix_remove:
+        if d.startswith(prefix):
+            d = d[len(prefix):]
+    return d
