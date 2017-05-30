ca69e17d9a843a01554482842c2150bb413773c9
Gaurav <gaurav@primenumbers.in>
2017-05-30 13:26:02 +0530

    Removing build lib

--- build/lib/pg_python/_delete.py
+++
@@ -1,7 +0,0 @@
-def make_postgres_delete_statement(table, kv_map, debug):
-    _prefix = "DELETE FROM "
-    keys = " and ".join([k + "=%s" for k in kv_map.keys()])
-    statement = " ".join([_prefix, table, " where ", keys])
-    if debug:
-        print("Writing into Db: %s, %s" % (statement, kv_map.values()))
-    return statement, kv_map.values()
--- build/lib/pg_python/pg_test.py
+++
@@ -1,4 +0,0 @@
-import pg_python
-
-pg_python.pg_server("crawler", "postgres", "@hawkerIndia", "postgres-master.hawker.news", False)
-pg_python.delete("xyz", {"key": "1000", "name": "2000"})
